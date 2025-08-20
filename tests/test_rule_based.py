08a6ce7a13e70e68812fc14bd6ad50d18435444a
M-Mahdi-Razmjoo <m.mahdi.razmjoo@gmail.com>
2025-08-21 00:14:59 +0330

    apply reviewer feedback

--- tests/test_rule_based.py
+++ tests/test_rule_based.py
@@ -1,135 +1,146 @@
 import pytest
-from tocount.rule_based import universal_tokens_estimator, openai_tokens_estimator_gpt_3_5, openai_tokens_estimator_gpt_4
-
+from tocount import estimate_text_tokens, TextEstimator
+from tocount.params import INVALID_TEXT_MESSAGE, INVALID_TEXT_ESTIMATOR_MESSAGE
 
 def test_universal_text_with_simple_prompt():
     message = "You are the text completion model"  # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=2
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(6, rel=0.8)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(6, abs=5)
 
 
 def test_universal_text_with_contractions():
     message = "I’m refining a foolproof method for reality shifting" # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=0
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(12, rel=0.8)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(12, abs=10)
 
 
 def test_universal_text_with_prefixes_and_suffixes(): # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=10
     message = "reflecting the hardships of the preparation process"
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(8, rel=3)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(8, abs=24)
 
 
 def test_universal_code_with_keywords():
     message1 = "def __init__(self, schema):" # http://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=19
-    assert isinstance(universal_tokens_estimator(message1), int)
-    assert universal_tokens_estimator(message1) == pytest.approx(9, rel=0.5)
+    assert isinstance(estimate_text_tokens(message1, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message1, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(9, abs=5)
 
     message2 = "class QueryPlanner:" # http://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=19
-    assert isinstance(universal_tokens_estimator(message2), int)
-    assert universal_tokens_estimator(message2) == pytest.approx(5, rel=0.3)
+    assert isinstance(estimate_text_tokens(message2, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message2, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(5, abs=2)
 
     message3 = """
     for op in operations:
         if op.type == "SELECT":
     """ # http://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=19
-    assert isinstance(universal_tokens_estimator(message3), int)
-    assert universal_tokens_estimator(message3) == pytest.approx(21, rel=0.4)
+    assert isinstance(estimate_text_tokens(message3, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message3, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(21, abs=9)
 
 
 def test_universal_code_with_variable_names():
     message = "table_name = ast.table_name" # http://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=19
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(9, rel=0.3)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(9, abs=3)
 
 
 def test_universal_text_empty_and_whitespace():
     message1 = ""
-    assert isinstance(universal_tokens_estimator(message1), int)
-    assert universal_tokens_estimator(message1) == 0
+    assert isinstance(estimate_text_tokens(message1, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message1, TextEstimator.RULE_BASED_UNIVERSAL) == 0
 
     message2 = " \t \n "
-    assert isinstance(universal_tokens_estimator(message2), int)
-    assert universal_tokens_estimator(message2) == pytest.approx(5,rel=5)
+    assert isinstance(estimate_text_tokens(message2, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message2, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(5, abs=25)
 
 
-def test_universal_non_english_with_special_chars():
+def test_universal_text_non_english_with_special_chars():
     message = "versión británica" #https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=13
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(7, rel=0.2)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(7, abs=2)
 
 
-def test_universal_non_english():
+def test_universal_text_non_english():
     message = "如何在sd上无错误进行模型训练"  # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=20
-    assert isinstance(universal_tokens_estimator(message), int)
-    assert universal_tokens_estimator(message) == pytest.approx(31, rel=0.6)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_UNIVERSAL) == pytest.approx(31, abs=19)
 
 
-def test_openai_heuristic_text_with_simple_prompt():
+def test_openai_GPT_3_5_text_with_simple_prompt():
     message = "You are the text completion model"  # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=2
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(6, rel=0.9)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(6, abs=6)
 
 
-def test_openai_heuristic_text_with_punctuation():
+def test_openai_GPT_3_5_text_with_punctuation():
     message = "Hey there! Are you familiar with reality shifting?" # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=4
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(10, rel=0.8)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(10, abs=8)
 
 
-def test_openai_heuristic_text_with_code_keywords():
+def test_openai_GPT_3_5_text_with_code_keywords():
     message = "if i ask in ten minutes will you still remember" # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=13
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(10, rel=0.7)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(10, abs=7)
 
 
-def test_openai_heuristic_text_with_long_word():
+def test_openai_GPT_3_5_text_with_long_word():
     message = "This is a verylongwordwithoutspaces and should be counted properly."
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(15, rel=0.7)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(15, abs=11)
 
 
-def test_openai_heuristic_text_with_url():
+def test_openai_GPT_3_5_text_with_url():
     message = "Analizza il contenuto di questo link https://www.deklasrl.com/siti-web-cosenza/" # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=29
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(31, rel=0.1)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(31, abs=4)
 
 
-def test_openai_heuristic_text_with_rare_character():
+def test_openai_GPT_3_5_text_with_rare_character():
     message = "What is the smallest possible value for P[A ∩ B ∩ C]?" # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=18
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(18, rel=0.2)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(18, abs=4)
 
 
-def test_openai_heuristic_text_with_newlines():
+def test_openai_GPT_3_5_text_with_newlines():
     message = "Line1\nLine2\nLine3"
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(8, rel=0.3)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(8, abs=3)
 
 
-def test_openai_heuristic_text_with_numbers():
+def test_openai_GPT_3_5_text_with_numbers():
     message = "doesnt it have 56 floors and 202 rooms" # https://huggingface.co/datasets/lmsys/lmsys-chat-1m?conversation-viewer=13
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(9, rel=0.6)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(9, abs=6)
 
 
-def test_openai_gpt4_model_adjustment():
+def test_openai_model_adjustment():
     message = "A simple sentence to compare models."
-    result_3_5 = openai_tokens_estimator_gpt_3_5(message)
-    result_4 = openai_tokens_estimator_gpt_4(message)
+    result_3_5 = estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5)
+    result_4 = estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_4)
 
     assert isinstance(result_3_5, int)
     assert isinstance(result_4, int)
     assert result_4 >= result_3_5
 
 
-def test_openai_gpt35_estimator_with_non_english():
+def test_openai_GPT_3_5_text_with_non_english():
     message = "如何在sd上无错误进行模型训练"  # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=20
-    assert isinstance(openai_tokens_estimator_gpt_3_5(message), int)
-    assert openai_tokens_estimator_gpt_3_5(message) == pytest.approx(31, rel=0.6)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_3_5) == pytest.approx(31, abs=19)
 
 
-def test_openai_gpt4_estimator_with_non_english():
+def test_openai_gpt_4_text_with_non_english():
     message = "如何在sd上无错误进行模型训练"  # https://huggingface.co/datasets/allenai/WildChat-1M?conversation-viewer=20
-    assert isinstance(openai_tokens_estimator_gpt_4(message), int)
-    assert openai_tokens_estimator_gpt_4(message) == pytest.approx(31, rel=0.6)
+    assert isinstance(estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_4), int)
+    assert estimate_text_tokens(message, TextEstimator.RULE_BASED_GPT_4) == pytest.approx(31, abs=19)
+
+
+def test_raises_error_for_invalid_text():
+    invalid_text = 12345
+    with pytest.raises(ValueError, match=INVALID_TEXT_MESSAGE): estimate_text_tokens(invalid_text)
+
+
+def test_raises_error_for_invalid_estimator():
+    valid_text = "sample prompt"
+    invalid_estimator = "not a valid estimator"
+    with pytest.raises(ValueError, match=INVALID_TEXT_ESTIMATOR_MESSAGE): estimate_text_tokens(valid_text, invalid_estimator)
