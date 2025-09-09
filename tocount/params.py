5cba4261c4da2dac8dceffac29b2dd72a7021889
M-Mahdi-Razmjoo <m.mahdi.razmjoo@gmail.com>
2025-09-09 15:08:57 +0330

    update params: remove INVALID_LINEAR_MODEL_MESSAGE

--- tocount/params.py
+++ tocount/params.py
@@ -5,7 +5,6 @@ TOCOUNT_VERSION = "0.1"
 
 INVALID_TEXT_ESTIMATOR_MESSAGE = "Invalid value. `estimator` must be an instance of TextEstimator enum."
 INVALID_TEXT_MESSAGE = "Invalid value. `text` must be a string."
-INVALID_LINEAR_MODEL_MESSAGE = "Invalid value. `model` must be a valid linear model name."
 
 LINEAR_MODELS = {
     "english": {"a": 0.22027472695240083, "b": 1.3098454987590542},
