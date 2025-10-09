156b11b217f9852b35e53861eb5f36cbf7fe863b
M-Mahdi-Razmjoo <m.mahdi.razmjoo@gmail.com>
2025-10-09 18:55:20 +0330

    add o200k tokenizer linear model

--- tocount/functions.py
+++ tocount/functions.py
@@ -8,6 +8,8 @@ from .tiktoken_r50k.functions import linear_tokens_estimator_all as r50k_linear_
 from .tiktoken_r50k.functions import linear_tokens_estimator_english as r50k_linear_english
 from .tiktoken_cl100k.functions import linear_tokens_estimator_all as cl100k_linear_all
 from .tiktoken_cl100k.functions import linear_tokens_estimator_english as cl100k_linear_english
+from .tiktoken_o200k.functions import linear_tokens_estimator_all as o200k_linear_all
+from .tiktoken_o200k.functions import linear_tokens_estimator_english as o200k_linear_english
 class _TextEstimatorRuleBased(Enum):
     """Rule based text token estimator enum."""
 
@@ -33,12 +35,21 @@ class _TextEstimatorTikTokenCL100K(Enum):
     DEFAULT = LINEAR_ENGLISH
 
 
+class _TextEstimatorTikTokenO200K(Enum):
+    """TikToken o200k text token estimator enum."""
+
+    LINEAR_ALL = "TIKTOKEN_O200K_LINEAR_ALL"
+    LINEAR_ENGLISH = "TIKTOKEN_O200K_LINEAR_ENGLISH"
+    DEFAULT = LINEAR_ENGLISH
+
+
 class TextEstimator:
     """Text token estimator class."""
 
     RULE_BASED = _TextEstimatorRuleBased
     TIKTOKEN_R50K = _TextEstimatorTikTokenR50K
     TIKTOKEN_CL100K = _TextEstimatorTikTokenCL100K
+    TIKTOKEN_O200K = _TextEstimatorTikTokenO200K
     DEFAULT = RULE_BASED.DEFAULT
 
 
@@ -50,6 +61,8 @@ text_estimator_map = {
     TextEstimator.TIKTOKEN_R50K.LINEAR_ENGLISH: r50k_linear_english,
     TextEstimator.TIKTOKEN_CL100K.LINEAR_ALL: cl100k_linear_all,
     TextEstimator.TIKTOKEN_CL100K.LINEAR_ENGLISH: cl100k_linear_english,
+    TextEstimator.TIKTOKEN_O200K.LINEAR_ALL: o200k_linear_all,
+    TextEstimator.TIKTOKEN_O200K.LINEAR_ENGLISH: o200k_linear_english,
 }
 
 
@@ -64,6 +77,6 @@ def estimate_text_tokens(text: str, estimator: TextEstimator = TextEstimator.DEF
     if not isinstance(text, str):
         raise ValueError(INVALID_TEXT_MESSAGE)
     if not isinstance(estimator, (
-        TextEstimator, _TextEstimatorRuleBased, _TextEstimatorTikTokenR50K, _TextEstimatorTikTokenCL100K)):
+        TextEstimator, _TextEstimatorRuleBased, _TextEstimatorTikTokenR50K, _TextEstimatorTikTokenCL100K, _TextEstimatorTikTokenO200K)):
         raise ValueError(INVALID_TEXT_ESTIMATOR_MESSAGE)
     return text_estimator_map[estimator](text)
